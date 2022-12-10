d768fbff3c60d021683f6eaf4bfda9919b900b3e
Irfan Asif <irfan_2020bece060@nitsri.net>
2022-12-10 22:07:40 +0530

    build: fetching data from backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
 import { Container } from '@chakra-ui/react';
+import axios from 'axios';
 import config from '../../config/general.config';
 import DashboardHeader from '../components/Dashboard/DashboardHeader';
 import LeaderBoardWrapper from '../components/Dashboard/LeaderBoardWrapper';
-import { dashboardDataType } from '../interfaces/dashboardStore';
 import SEO from '../components/SEO/SEO';
-import { dataCalculator } from '../util/dataCalculator';
+import { dashboardDataType } from '../interfaces/dashboardStore';
 
 export default function Home(props: {
   dashboardData: dashboardDataType[];
   bountyDataJson: any;
 }) {
   const { dashboardData } = props;
-
+  console.log('dash data - ', dashboardData.length);
   return (
     <>
       <SEO
@@ -30,13 +30,28 @@ export default function Home(props: {
 }
 
 export async function getStaticProps() {
-  const { personData, bountyDataJson } = await dataCalculator();
-
-  return {
-    props: {
-      bountyDataJson,
-      dashboardData: personData,
-    },
-    revalidate: 10,
-  };
+  // fetch personData, bountyDataJson from process.env.BACKEND_URL using axios
+  return axios
+    .get(`${process.env.BACKEND_URL}/xp`)
+    .then(async (res) => { 
+      const personData = res.data.personData;
+      const bountyDataJson = res.data.bountyDataJson;
+      return {
+        props: {
+          bountyDataJson,
+          dashboardData: personData,
+        },
+        revalidate: 10,
+      };
+    })
+    .catch((err) => {
+      console.log('err -> ', err);
+      return {
+        props: {
+          bountyDataJson: {},
+          dashboardData: {},
+        },
+        revalidate: 10,
+      };
+    });
 }
