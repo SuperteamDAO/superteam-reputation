5a3b31d78f2f512aedfe9458717f3595861a6519
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-12-17 18:28:28 +0530

    refactor: navbar according to new design

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -1,51 +1,35 @@
 import {
   Button,
-  chakra,
   Container,
   Flex,
   Icon,
   Stack,
+  Text,
   useColorMode,
   useColorModeValue,
-  VisuallyHidden,
 } from '@chakra-ui/react';
-import { ReactNode } from 'react';
+import Link from 'next/link';
 import { BsFillMoonFill, BsSunFill } from 'react-icons/bs';
-import { FaDiscord, FaTwitter } from 'react-icons/fa';
-import { SiSubstack } from 'react-icons/si';
 import LogoImg from '../Logo/LogoImg';
 
-const IconButton = ({
-  children,
-  label,
-  href,
-}: {
-  children: ReactNode;
-  label: string;
-  href: string;
-}) => {
-  return (
-    <chakra.button
-      w={8}
-      h={8}
-      color={useColorModeValue('superteamGreyDT.100', 'superteamGreyLT.600')}
-      cursor={'pointer'}
-      as={'a'}
-      href={href}
-      target={'_blank'}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'color 0.3s ease'}
-      _hover={{
-        color: useColorModeValue('superteamGreyDT.500', 'superteamGreyLT.100'),
-      }}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  );
-};
+const NavbarLinks = [
+  {
+    name: 'Reputation System',
+    href: '#',
+  },
+  {
+    name: 'Projects',
+    href: '#',
+  },
+  {
+    name: 'Bounties',
+    href: '#',
+  },
+  {
+    name: 'GDP',
+    href: '#',
+  },
+];
 
 export default function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -59,7 +43,10 @@ export default function Header() {
       )}
       p="0.8rem"
       borderBottom={'1px solid '}
-      borderColor={useColorModeValue('superteamGreyLT.500', 'superteamGreyDT.50')}
+      borderColor={useColorModeValue(
+        'superteamGreyLT.500',
+        'superteamGreyDT.50'
+      )}
     >
       <Flex
         flexDir={'row'}
@@ -70,30 +57,47 @@ export default function Header() {
         maxW="7xl"
       >
         <LogoImg />
-        <Stack direction={'row'} spacing={[2, 6]}>
-          <IconButton
-            label={'Discord'}
-            href={'https://discord.com/invite/Mq3ReaekgG'}
+        <Stack
+          color={useColorModeValue(
+            'superteamGreyDT.100',
+            'superteamGreyLT.600'
+          )}
+          direction={'row'}
+          spacing={[2, 6]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          {NavbarLinks.map((link, key) => {
+            return (
+              <Link passHref key={key} href={link.href}>
+                <Text>{link.name}</Text>
+              </Link>
+            );
+          })}
+          <Button
+            bg={useColorModeValue('superteamBlueLT.800', 'superteamOrange.800')}
+            color="white"
+            variant={'unstyled'}
+            w="8rem"
           >
-            <Icon as={FaDiscord} w={[5, 6]} h={[5, 6]} />
-          </IconButton>
-          <IconButton
-            label={'Twitter'}
-            href={'https://twitter.com/superteamdao'}
-          >
-            <Icon as={FaTwitter} w={[5, 6]} h={[5, 6]} />
-          </IconButton>
-          <IconButton
-            label={'Substack'}
-            href={'https://superteam.substack.com/p'}
-          >
-            <Icon as={SiSubstack} w={[4, 5]} h={[4, 5]} />
-          </IconButton>{' '}
+            Submit XP
+          </Button>
           <Button variant={'unstyled'} onClick={toggleColorMode}>
             {colorMode === 'light' ? (
-              <Icon as={BsFillMoonFill} color={'superteamGreyDT.100'} w={[4, 5]} h={[4, 5]} />
+              <Icon
+                as={BsFillMoonFill}
+                color={'superteamGreyDT.100'}
+                w={[4, 5]}
+                h={[4, 5]}
+              />
             ) : (
-              <Icon as={BsSunFill} color={'superteamGreyLT.600'} w={[4, 5]} h={[4, 5]} />
+              <Icon
+                as={BsSunFill}
+                color={'superteamGreyLT.600'}
+                w={[4, 5]}
+                h={[4, 5]}
+              />
             )}
           </Button>
         </Stack>
