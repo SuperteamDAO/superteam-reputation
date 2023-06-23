bc9d411820719ba6a8a47fd66cf6c09ba318d18a
Deepak <deepakkomma@gmail.com>
2023-06-23 18:05:59 +0530

    feat: added airtable data interface

--- src/pages/api/airtableData.ts
+++ src/pages/api/airtableData.ts
@@ -1,39 +1,62 @@
-import { FieldSet, Table } from 'airtable';
-import { AirtableBase } from 'airtable/lib/airtable_base';
+import { FieldSet } from 'airtable';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getDatabase } from '../../util/getDatabase';
+
+
+
+
+export interface AirtableResponse {
+  id: string;
+  fields: FieldSet
+}
+
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<AirtableResponse[]>
 ) {
 
-  const data: any[] = [];
-  const base: AirtableBase = getDatabase();
-  const table: Table<FieldSet> = base("XP Summary");
-  table.select({
+  const data: AirtableResponse[] = [];
+  const base = getDatabase();
+  const table = base("XP Summary");
+  const records = await table.select({
     // Selecting the first 3 records in XP by skill:
     // maxRecords: 10,
     fields: ["Name", "Person Type", "Region", "Total XP", "XP (Project)", "XP (Indie)", "Ops XP", "CAB XP", "Bounty XP",],
     view: "XP by work type"
-  }).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-    let n = 0;
-    records.forEach(function (record) {
-      n++;
-      // console.log('Retrieved', record.fields);
-      data.push(record.fields);
-    });
-    console.log("Total number of contributers ", n)
+  }).all();
 
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
-    res.json(data)
+  const recordsCleaned = records.map((record) => ({
+    id: record.id,
+    fields: record.fields,
+  }));
 
-  }, function done(err) {
-    if (err) { console.error(err); return; }
-  });
+
+  console.log("Total number of records obtained are ", recordsCleaned.length as number)
+  // const recordData = JSON.stringify(recordsCleaned, null, 2);
+
+  res.status(200).json(recordsCleaned);
+
+
+  //   .eachPage(function page(records, fetchNextPage) {
+  //   // This function (`page`) will get called for each page of records.
+  //   let n = 0;
+  //   records.forEach(function (record) {
+  //     n++;
+  //     // console.log('Retrieved', record.fields);
+  //     data.push(record.fields);
+  //   });
+  //   console.log("Total number of contributers ", n)
+
+  //   // To fetch the next page of records, call `fetchNextPage`.
+  //   // If there are more records, `page` will get called again.
+  //   // If there are no more records, `done` will get called.
+  //   fetchNextPage();
+  //   res.json(data)
+
+  // }, function done(err) {
+  //   if (err) { console.error(err); return; }
+  // });
 
 
 }
